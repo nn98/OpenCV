189a3fc77222be9524e5b827febdc4c4c509dc39
nn98 <jkllhgb@gmail.com>
2020-04-05 20:18:16 +0900

    test 3

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,21 @@
 package test;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+        //Load native library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        //image container object
+        Mat imageArray;
+        //Read image from file system
+        imageArray=Highgui.imread("C:\\Users\\mesutpiskin\\sample.jpg");
+        //Get image with & height
+        System.out.println(imageArray.rows());
+        System.out.println(imageArray.cols());
+    }
 
 }
